40b8bafba809f1641bebf9b1efc48214d20b522e
Daniel <danielalexander03@icloud.com>
2025-05-23 15:54:46 -0500

    CSS

--- src/components/Autor.jsx
+++ src/components/Autor.jsx
@@ -5,13 +5,15 @@ import './Autor.css';
 const Autor = ({autor}) => {
   return (
     <div className="col-md-4 mb-4 text-center justify-content-center">
-        <div className="card card-width h-100 justify-content-center">
+        <div className="card card-width h-100 w-100 justify-content-center">
         <img className= "foto_autor card-img-top mx-auto d-block" src={autor.foto} alt={`Foto de ${autor.nombre_autor}`} />
         <div className="card-body">
           <h1 className="card-title card_titulo_autor">{autor.nombre_autor}</h1>
-          <p className="card-text">Número de libros: {autor.numero_libros}</p>
-          <p className="card-text">País: {autor.nacionalidad}</p>
-          <Link to={`/autor/${autor.id_autor}`} className="btn btn-primary">Ver {autor.nombre_autor}</Link>
+          <h4 className="edad">Edad: {autor.edad}</h4>
+          <p className="card-text"><strong>Número de libros:</strong> {autor.numero_libros}</p>
+          <p className="card-text"><strong>País:</strong> {autor.nacionalidad}</p>
+          <p className="card-text"><strong>Biografía:</strong> {autor.biografia}</p>
+
         </div>
       </div>
     </div>
