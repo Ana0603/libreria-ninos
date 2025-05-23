fea267415cedba5eb62f1e894526ba7e4d473189
santysos <santysos@hotmail.com>
2025-05-23 16:11:17 -0500

    solucion error

--- src/views/Landing_Autores.jsx
+++ src/views/Landing_Autores.jsx
@@ -1,27 +1,34 @@
 import Autor from "../components/Autor";
 import useAutores from "../hooks/useAutores.js";
 
-
 const Vista_autores = () => {
-  const { autoresData, loading } = useAutores();
+  const { autoresData, loading, error } = useAutores();
+
   if (loading) {
-    return <div className="container-fluid d-flex justify-content-center">
-     <div className="spinner-grow" role="status">
-</div>
-</div>
+    return (
+      <div className="container-fluid d-flex justify-content-center">
+        <div className="spinner-grow" role="status"></div>
+      </div>
+    );
   }
+
   if (error) {
-    return <div className="container mt-4"><h1>Error al cargar los autores</h1></div>;
+    return (
+      <div className="container mt-4">
+        <h1>Error al cargar los autores</h1>
+      </div>
+    );
   }
 
   return (
     <div className="container mt-4">
-     <div className="row">
+      <div className="row">
         {autoresData.map((autor) => (
-          <Autor key={autor.id} autor={autor}/>
+          <Autor key={autor.id} autor={autor} />
         ))}
       </div>
-      </div>
+    </div>
   );
-}
+};
+
 export default Vista_autores;
