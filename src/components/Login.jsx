2bfda356cd676fd088cc1fe466e7c09fe21c7b44
SebasGar135 <sebasgarzon135@gmail.com>
2025-05-23 08:37:32 -0500

    Cambios Login
    Boton registro
    Iniciar con Google
    Estilos
    Recuerdame

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import './Login.css';
 
 function Login({ onLogin }) {
   const [usuario, setUsername] = useState("");
   const [clave, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [recuerdame, setRecuerdame] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,24 +18,49 @@ function Login({ onLogin }) {
   };
 
   return (
-    <div>
-      <h2>Iniciar Sesión</h2>
-      <form onSubmit={handleSubmit}>
-        <input
+    <div className="login-container">
+      <h2 className="login-titulo">Iniciar Sesión</h2>
+      <form className="formulario-login" onSubmit={handleSubmit}>
+        <input className="input-usuario"
           type="text"
           placeholder="Usuario"
           value={usuario}
           onChange={(e) => setUsername(e.target.value)}
         />
-        <input
+        <input className="input-clave"
           type="password"
           placeholder="Contraseña"
           value={clave}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Entrar</button>
+        <div>
+          <label>
+            <input className="input-checkbox"
+              type="checkbox"
+              checked={recuerdame}
+              onChange={(e) => setRecuerdame(e.target.checked)}
+            />
+            Recuérdame
+          </label>
+        </div>
+        <button className="btn-inSesion" type="submit">Iniciar Sesión</button>
         {error && <p style={{ color: "red" }}>{error}</p>}
       </form>
+      <div style={{ marginTop: "1rem" }}>
+        <button className="btn-google"
+          style={{
+            background: "#fff",
+            color: "#444",
+            border: "1px solid #ccc",
+            padding: "0.5rem 1rem",
+            marginBottom: "0.5rem",
+          }}
+        >
+          Inicar con Google
+        </button>
+        <br />
+        <a className="registro" href="/registro">¿No tienes cuenta? Regístrate</a>
+      </div>
     </div>
   );
 }
